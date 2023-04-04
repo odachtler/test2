f5822379089879404eedcd223e2e8a8935425692
Olivia Dachtler <olivia-dachtler@uiowa.edu>
2023-04-04 09:49:43 -0500

    security key changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for, flash
 
 app = Flask(__name__)
+app.config(SECRET_KEY="odfkbkfs")
 
 books_list = [
     {"title": "The Song of Achilles", "author": "Madeline Miller ", "pages": "416", "classification": "fiction", "details": "read, recommend", "acquisition": "loaned"}
@@ -74,6 +75,3 @@ def about():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-app.config.update(SECRET_KEY="odfkbkfs")
